5165a26ba60a94796ed38619ab5777a8181e1c8b
unknown <rangasaiy33@gmail.com>
2021-06-19 01:06:34 +0530

    Inital commit

---
+++ src/main/java/com/pvj/PVJMain.java
@@ -0,0 +1,10 @@
+package com.pvj;
+
+import org.springframework.boot.SpringApplication;
+
+public class PVJMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PVJMain.class, args);
+	}
+}
