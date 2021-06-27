839d17ddbb174604b8c340fa92ff00ec632f2d7f
unknown <rangasaiy33@gmail.com>
2021-06-27 21:23:07 +0530

    directly add value to db

--- src/main/java/com/pvj/db/DatabaseConfig.java
+++ src/main/java/com/pvj/db/DatabaseConfig.java
@@ -12,8 +12,8 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
+    //@Value("${spring.datasource.url}")
+    private String dbUrl = "jdbc:mysql://bdde21b7cf435e:9c6eeaa8@us-cdbr-east-04.cleardb.com/heroku_9bf29f35787134a?reconnect=true";
 
     @Bean
     public DataSource dataSource() {
