ba955b827c09db6586c25aa93834320856b0972f
unknown <rangasaiy33@gmail.com>
2021-06-27 23:17:55 +0530

    added

--- src/main/java/com/pvj/db/DatabaseConfig.java
+++ src/main/java/com/pvj/db/DatabaseConfig.java
@@ -2,17 +2,23 @@ package com.pvj.db;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 public class DatabaseConfig {
+
+    
+    private String dbUrl="jdbc:mysql://bdde21b7cf435e:9c6eeaa8@us-cdbr-east-04.cleardb.com/heroku_9bf29f35787134a?reconnect=true";
+
     @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
-        return new org.apache.tomcat.jdbc.pool.DataSource();
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
     }
 }
