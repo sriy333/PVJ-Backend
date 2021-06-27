ee159383facaa2b632cc94cb628b355ec74e8fcd
unknown <rangasaiy33@gmail.com>
2021-06-27 22:30:12 +0530

    commit

--- src/main/java/com/pvj/db/DatabaseConfig.java
+++ src/main/java/com/pvj/db/DatabaseConfig.java
@@ -5,11 +5,13 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
+@Component
 public class DatabaseConfig {
 
     //@Value("${spring.datasource.url}")
