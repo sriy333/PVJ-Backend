5c8d9b8c68757851d2209a4f67b53b75eb503799
unknown <rangasaiy33@gmail.com>
2021-06-27 22:03:23 +0530

    main file changes

--- src/main/java/com/pvj/PVJMain.java
+++ src/main/java/com/pvj/PVJMain.java
@@ -3,9 +3,10 @@ package com.pvj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class PVJMain {
 
 	public static void main(String[] args) {
