acddad54e8594310b9194e1f9cd1bcee45cd488f
unknown <rangasaiy33@gmail.com>
2021-06-28 22:38:59 +0530

    changes

--- src/main/java/com/pvj/db/JpaConfig.java
+++ src/main/java/com/pvj/db/JpaConfig.java
@@ -1,14 +1,9 @@
 package com.pvj.db;
 
-import java.io.PrintWriter;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
-import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
