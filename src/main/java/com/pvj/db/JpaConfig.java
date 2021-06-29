9d66b30fb59f7935209fb91c59c05f1a75d66d13
unknown <rangasaiy33@gmail.com>
2021-06-29 21:35:56 +0530

    commit

--- src/main/java/com/pvj/db/JpaConfig.java
+++ src/main/java/com/pvj/db/JpaConfig.java
@@ -1,25 +1,25 @@
-package com.pvj.db;
-
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
-@Configuration
-@Component
-public class JpaConfig extends HikariConfig {
-
-    @Bean
-    public DataSource dataSource() throws SQLException {
-    	HikariDataSource config = new HikariDataSource();
-    	config.setJdbcUrl("jdbc:mysql://bdde21b7cf435e:9c6eeaa8@us-cdbr-east-04.cleardb.com/heroku_9bf29f35787134a?reconnect=true");
-        return config;
-    }
-
-}
+//package com.pvj.db;
+//
+//import java.sql.SQLException;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.stereotype.Component;
+//
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//
+//@Configuration
+//@Component
+//public class JpaConfig extends HikariConfig {
+//
+//    @Bean
+//    public DataSource dataSource() throws SQLException {
+//    	HikariDataSource config = new HikariDataSource();
+//    	config.setJdbcUrl("jdbc:mysql://bdde21b7cf435e:9c6eeaa8@us-cdbr-east-04.cleardb.com/heroku_9bf29f35787134a?reconnect=true");
+//        return config;
+//    }
+//
+//}
